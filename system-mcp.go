040a57d28d29f632c2e8a64558ca9b04989fa0e6
sirui <srandroid@163.com>
2025-05-24 15:49:47 +0800

    优化

--- system-mcp.go
+++ system-mcp.go
@@ -22,7 +22,7 @@ func main() {
 		server.WithRecovery(),
 	)
 
-	s.AddTools(tool.CreateFile())
+	s.AddTools(tool.CreateFile(), tool.ReadDirFile())
 
 	if err := server.ServeStdio(s); err != nil {
 		fmt.Printf("MCP Server错误: %v\n", err)
